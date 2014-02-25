120801ad26b0ac0a9a542a3720896afe5aee501b
Edwin Shao <edwin.shao@oracle.com>
2014-02-25 01:18:14 -0800

    new2

--- TestingLearning/test.rb
+++ TestingLearning/test.rb
@@ -1,3 +1,3 @@
-Tnew222his is from branch2, probabaly confilication
+Tn888ew2888822his is from branch2, probabaly confilication
 This is from branch1, you need to take a lo
 This is newest line here
